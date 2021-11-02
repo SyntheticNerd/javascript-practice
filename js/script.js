5d865ca8e2e1e2e3c834fb32a015d6ae4b5203c9
SyntheticDesigner <andrew.sch.j@gmail.com>
2021-11-01 20:33:31 -0700

    Added the multi line comment

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 //debugger; good for stepping through logic
 console.log("Andrew Schroepfer");
-console.log("Apple Pie");
-
+console.log("Apple Pie"/* multi line comment
+multi line comment
+multi line comment
+multi line comment*/
+);
 //white space is not meaning ful in JS
 //naming anything JS is case sensitive
 //TODO: Make notes comments can be used by other programs like CANBAN and github
