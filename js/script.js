81adfb3f5194890a3a447fdd8f0b88d3fe6c7646
SyntheticDesigner <andrew.sch.j@gmail.com>
2021-11-01 20:28:55 -0700

    take-home day 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("hello World");
+//debugger; good for stepping through logic
+console.log("Andrew Schroepfer");
+console.log("Apple Pie");
+
+//white space is not meaning ful in JS
+//naming anything JS is case sensitive
+//TODO: Make notes comments can be used by other programs like CANBAN and github
+//Semi-Colons are optional?
+let element = document.getElementById('hello');
