57694cad88df0b8fdcefe87fbcece492837a2c57
SyntheticDesigner <andrew.sch.j@gmail.com>
2021-11-03 19:11:10 -0700

    day two notes

--- js/script.js
+++ js/script.js
@@ -12,3 +12,15 @@ multi line comment*/
 //TODO: Make notes comments can be used by other programs like CANBAN and github
 //Semi-Colons are optional?
 let element = document.getElementById('hello');
+
+/*let name = "Andrew";
+greeting = `hello ${name}`;
+greeting.length
+greeting.padStart(25, 'H');
+greeting.split(' ');
+typeof(name); //get the type of the variable
+Number.isFinite(variable)// check the validity of the number make sure its not infinite
+//always check for null and undefined values
+let bigOne = 1n;
+typeof(bigOne); 
+//big int stores big numbers... */
