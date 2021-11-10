2757a00ff30306fad1a1d99eafe998b99023f8f5
SyntheticDesigner <andrew.sch.j@gmail.com>
2021-11-09 21:40:22 -0800

    updated day 3 take home with function and Confirm() function

--- js/day-3-take-home.js
+++ js/day-3-take-home.js
@@ -14,40 +14,56 @@ function doFunction() {
     console.log(firstname);
     alert(lastname);
 
-
     var bday = prompt("What is your Birthday", "00/00/0000")
-
     while (!bday.match("^\\d{2}/\\d{2}/\\d{4}")) {
-        var bday = prompt("Incorect format try again", "00/00/0000")
+        bday = prompt("Incorect format try again", "00/00/0000")
     } //super annoying website practice
+    confirmit();
+    //call the confirm function
 
-    var bdate = new Date(bday);
-    //could have stopped here but I didnt want to alert to have the seconds
+    function askbday() {
+        bday = prompt("What is your Birthday", "00/00/0000")
 
-    var monthName = {
-        month: 'long'
-    };
-    var month1 = Intl.DateTimeFormat('en-US', monthName).format(bdate);
-    //this is the method for getting the long form of the months otherwise months will be (0-11)
-    var date = bdate.getDate();
-    //getDate works fine for the day
-
-    console.log("almost there");
-    var wd = {
-        weekday: 'long'
-    };
-    //had to use a similar method to get the name oif the day otherwise you get a number (0-6)
-    var day = Intl.DateTimeFormat('en-US', wd).format(bdate);
-    var year = bdate.getFullYear();
-
-    alert("Your Birthday is: " + day + " " + month1 + " " + date + " " + year);
-
-    var today = new Date();
-    var age = today.getFullYear() - bdate.getFullYear();
-    var m = today.getMonth() - bdate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
+        while (!bday.match("^\\d{2}/\\d{2}/\\d{4}")) {
+            var bday = prompt("Incorect format try again", "00/00/0000")
+        }
+        console.log("you are here");
+        confirmit();
     }
+    //create a function to call if confirm fails
 
-    alert("You are " + age + " years old");
+    function confirmit() {
+        if (confirm("You Entered: " + bday + " is this correct?")) {
+            var bdate = new Date(bday);
+            //could have stopped here but I didnt want to alert to have the seconds
+
+            var monthName = {
+                month: 'long'
+            };
+            var month1 = Intl.DateTimeFormat('en-US', monthName).format(bdate);
+            //this is the method for getting the long form of the months otherwise months will be (0-11)
+            var date = bdate.getDate();
+            //getDate works fine for the day
+            var wd = {
+                weekday: 'long'
+            };
+            //had to use a similar method to get the name oif the day otherwise you get a number (0-6)
+            var day = Intl.DateTimeFormat('en-US', wd).format(bdate);
+            var year = bdate.getFullYear();
+
+            alert("Your Birthday is: " + day + " " + month1 + " " + date + " " + year);
+
+            var today = new Date();
+            var age = today.getFullYear() - bdate.getFullYear();
+            var m = today.getMonth() - bdate.getMonth();
+            if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+                age--;
+            }
+
+            alert("You are " + age + " years old");
+        } else {
+            askbday();
+        }
+    }
+    //created a confirm funnction to call in the askbday function
 }
